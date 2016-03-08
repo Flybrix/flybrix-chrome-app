5831ad0092435600f5cc5285e2bdab93ee352520
Robb Walters <robb@senecameras.com>
2016-03-07 20:24:32 -0800

    roll version number on chrome side

--- js/eeprom-config.js
+++ js/eeprom-config.js
@@ -1,4 +1,4 @@
-var flybrix_app_configuration_version = [1,0,0]; //checked at startup!
+var flybrix_app_configuration_version = [1,1,0]; //checked at startup!
 
 var eepromConfig = {
 	version : [0.0, 0.0, 0.0],
