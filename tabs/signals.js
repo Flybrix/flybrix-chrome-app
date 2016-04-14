fa3c9d73a1c4651214d406368d4cc8309e603f39
Adnan Ademovic <adnanademovic100@gmail.com>
2016-04-15 00:17:41 +0200

    Transition the signals tab to AngularJS

--- tabs/signals.js
+++ tabs/signals.js
@@ -2,55 +2,17 @@
 // order of channels in assignedChannel array is ['THROTTLE','PITCH','ROLL','YAW/RUDDER','AUX 1','AUX 2']
 
 function initialize_signals_view() {
-    $('#commands-master-plot').create_plot(["Fz (m)", "Tx (deg)", "Ty (deg)", "Tz (deg)"]);
-    $('#commands-slave-plot').create_plot(["Fz (m/s)", "Tx (deg/s)", "Ty (deg/s)", "Tz (deg/s)"]);
-    $('#commands-direct-plot').create_plot(["Fz (pwm counts)", "Tx (pwm counts)", "Ty (pwm counts)", "Tz (pwm counts)"]);
-
-    $('#signals .command-settings-channelMidpoint-field').connect_to_eeprom();
-    $('#signals .command-settings-channelDeadzone-field').connect_to_eeprom();
-    $('#signals .command-settings-assignedChannel-field').connect_to_eeprom();
-    $('#signals .command-settings-commandScaling-field').connect_to_eeprom();
-    $('#signals .command-settings-checkbox-inversion').connect_to_eeprom();
     eeprom_refresh_callback_list.add(refresh_signals_view_from_eepromConfig);
 
     refresh_signals_view_from_eepromConfig();
-};
-
-function refresh_signals_view_from_eepromConfig() {
-    loadArrayValues($('#signals .command-settings-assignedChannel-field'), eepromConfig.assignedChannel, 0);
-
-    $("#signals .command-settings-checkbox-inversion.0").prop("checked", ((eepromConfig.commandInversion >> 0) & 1));
-    $("#signals .command-settings-checkbox-inversion.1").prop("checked", ((eepromConfig.commandInversion >> 1) & 1));
-    $("#signals .command-settings-checkbox-inversion.2").prop("checked", ((eepromConfig.commandInversion >> 2) & 1));
-
-    $("#signals .command-settings-commandScaling-field.thrustMasterPIDParameters ").val(eepromConfig.thrustMasterPIDParameters[6].toFixed(4));
-    $("#signals .command-settings-commandScaling-field.pitchMasterPIDParameters ").val(eepromConfig.pitchMasterPIDParameters[6].toFixed(4));
-    $("#signals .command-settings-commandScaling-field.rollMasterPIDParameters ").val(eepromConfig.rollMasterPIDParameters[6].toFixed(4));
-    $("#signals .command-settings-commandScaling-field.yawMasterPIDParameters ").val(eepromConfig.yawMasterPIDParameters[6].toFixed(4));
-    $("#signals .command-settings-commandScaling-field.thrustSlavePIDParameters ").val(eepromConfig.thrustSlavePIDParameters[6].toFixed(4));
-    $("#signals .command-settings-commandScaling-field.pitchSlavePIDParameters ").val(eepromConfig.pitchSlavePIDParameters[6].toFixed(4));
-    $("#signals .command-settings-commandScaling-field.rollSlavePIDParameters ").val(eepromConfig.rollSlavePIDParameters[6].toFixed(4));
-    $("#signals .command-settings-commandScaling-field.yawSlavePIDParameters ").val(eepromConfig.yawSlavePIDParameters[6].toFixed(4));
-
-    $("#signals .command-settings-channelMidpoint-field.0 ").val(eepromConfig.channelMidpoint[0].toFixed(0));
-    $("#signals .command-settings-channelMidpoint-field.1 ").val(eepromConfig.channelMidpoint[1].toFixed(0));
-    $("#signals .command-settings-channelMidpoint-field.2 ").val(eepromConfig.channelMidpoint[2].toFixed(0));
-    $("#signals .command-settings-channelMidpoint-field.3 ").val(eepromConfig.channelMidpoint[3].toFixed(0));
-    $("#signals .command-settings-channelMidpoint-field.4 ").val(eepromConfig.channelMidpoint[4].toFixed(0));
-    $("#signals .command-settings-channelMidpoint-field.5 ").val(eepromConfig.channelMidpoint[5].toFixed(0));
-
-    $("#signals .command-settings-channelDeadzone-field.0 ").val(eepromConfig.channelDeadzone[0].toFixed(0));
-    $("#signals .command-settings-channelDeadzone-field.1 ").val(eepromConfig.channelDeadzone[1].toFixed(0));
-    $("#signals .command-settings-channelDeadzone-field.2 ").val(eepromConfig.channelDeadzone[2].toFixed(0));
-    $("#signals .command-settings-channelDeadzone-field.3 ").val(eepromConfig.channelDeadzone[3].toFixed(0));
-    $("#signals .command-settings-channelDeadzone-field.4 ").val(eepromConfig.channelDeadzone[4].toFixed(0));
-    $("#signals .command-settings-channelDeadzone-field.5 ").val(eepromConfig.channelDeadzone[5].toFixed(0));
 }
+
+function refresh_signals_view_from_eepromConfig(){}
 
 (function() {
     'use strict';
 
-    var signalsController = function($scope, $rootScope) {
+    var signalsController = function($scope, $rootScope, deviceConfig) {
         $rootScope.$watch('state', function(st) {
             if (!st)
                 return;
@@ -103,8 +65,23 @@ function refresh_signals_view_from_eepromConfig() {
 
             $scope.outputLevels = [Fz_cmd, Tx_cmd, Ty_cmd, Tz_cmd];
         });
+
+        $scope.inversions = [0, 0, 0];
+
+        $scope.onInversionChange = function() {
+            $rootScope.eepromConfig.commandInversion = $scope.inversions.reduce(function(acc, val, idx) {
+                return acc + (val ? (1 << idx) : 0);
+            }, 0);
+            deviceConfig.send($rootScope.eepromConfig);
+        };
+
+        $rootScope.$watch('eepromConfig.commandInversion', function(value) {
+            $scope.inversions.forEach(function(val, idx) {
+                $scope.inversions[idx] = (value & (1 << idx)) != 0;
+            });
+        });
     };
 
-    angular.module('flybrixApp').controller('signalsController', ['$scope', '$rootScope', signalsController]);
+    angular.module('flybrixApp').controller('signalsController', ['$scope', '$rootScope', 'deviceConfig', signalsController]);
 
 }());
