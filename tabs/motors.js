8881d09ee4e5505ca14420acea856111f163906d
Adnan Ademovic <adnanademovic100@gmail.com>
2016-04-18 17:30:24 +0200

    Move the motors buttons to AngularJS

--- tabs/motors.js
+++ tabs/motors.js
@@ -1,94 +1,93 @@
 
 function initialize_motors_view() {
-	$('#motor-view-override-pilot').click(function() {
-        var style_str = $('#motor-view-motors-override').attr('style');
-        if (style_str && ($.inArray('background-color', style_str.split(':')) >= 0)) {
-            setTimeout(function() {	send_message(CommandFields.COM_SET_COMMAND_OVERRIDE, 1); },1);
-        }
-        else{
-            setTimeout(function() {	send_message(CommandFields.COM_SET_COMMAND_OVERRIDE, 0); }, 1);
-        }
-    });
-
-	$('#motor-view-enable-motors').click(function() {
-
-        var style_str = $('#motor-view-motors-enabled').attr('style');
-        if (style_str && ($.inArray('background-color', style_str.split(':')) >= 0)) {
-            console.log('enabling motors');
-            var i;
-            for (i = 0; i < 81; i++) {
-                setTimeout(function() {	send_message(CommandFields.COM_REQ_ENABLE_ITERATION, 1, false); }, i*10);
-            }
-            setTimeout(function() {	send_message(CommandFields.COM_REQ_ENABLE_ITERATION, 1); }, i);
-        }
-        else{
-            console.log('disabling motors');
-            setTimeout(function() {	send_message(CommandFields.COM_REQ_ENABLE_ITERATION, 0); }, 1);
-        }
-    });
-
-    $('#motor-view-zero-motors').click(function() {
-        console.log('zero');
-        setTimeout(function(){send_message(CommandFields.COM_MOTOR_OVERRIDE_SPEED_ALL, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]); }, 1);
-    });
-
-    $('#motors .motor-view-level-value').change(function(e){
-        var motor_n = parseInt($(this).parent().index()); // motor number
-        console.log(motor_n);
-        var motor_v = parseInt($(this).val()); // motor value
-        setTimeout(function(){send_message(CommandFields.COM_MOTOR_OVERRIDE_SPEED_0 << motor_n, [motor_v % 256, motor_v / 256]); }, 1);
-    }).attr('step', 'any').attr('type', 'number').attr('min', 0);
+    $('#motors .motor-view-level-value')
+        .change(function(e) {
+            var motor_n = parseInt($(this).parent().index());  // motor number
+            console.log(motor_n);
+            var motor_v = parseInt($(this).val());  // motor value
+            setTimeout(function() {
+                send_message(CommandFields.COM_MOTOR_OVERRIDE_SPEED_0 << motor_n, [motor_v % 256, motor_v / 256]);
+            }, 1);
+        })
+        .attr('step', 'any')
+        .attr('type', 'number')
+        .attr('min', 0);
 
     $('#motors .motor-view-level-value').blur();
 };
 
-function update_bar_css(index, type, val){
-    // if newval = +4096 --> top is 0, height is 128
-    // if newval = -2047 --> top is 128, height is 64
-    var top_px    = 128;
-    var height_px = 1;
-    if (val > 4) {
-        height_px = val * 128/4096;
-        top_px = 128 - height_px;
-    }
-    if (val < -4) {
-        height_px = val * -128/4096;
-        top_px = 128;
-    }
-    $('.'+ index +' .motor-view-level-bar-' + type).css({'top' : top_px+'px','height' : height_px+'px'});
-}
+
 
 (function() {
-  'use strict';
+    'use strict';
 
-  var motorsController = function ($scope, $rootScope) {
-      var last_motors_view_update = new Date();
-      $scope.$watch('state', function () {
-          if (!$rootScope.state)
-              return;
+    var motorsController = function($scope, $rootScope, $interval, serial) {
+        function update_bar_css(index, type, val) {
+            // if newval = +4096 --> top is 0, height is 128
+            // if newval = -2047 --> top is 128, height is 64
+            var top_px = 128;
+            var height_px = 1;
+            if (val > 4) {
+                height_px = val * 128 / 4096;
+                top_px = 128 - height_px;
+            }
+            if (val < -4) {
+                height_px = val * -128 / 4096;
+                top_px = 128;
+            }
+            $('.' + index + ' .motor-view-level-bar-' + type).css({'top': top_px + 'px', 'height': height_px + 'px'});
+        }
 
-          $scope.motorsEnabledNeg = !($rootScope.state.status & 0x0400);
-          $scope.motorsOverrideNeg = !($rootScope.state.status & 0x8000);
 
-          var now = new Date();
-          if (now - last_motors_view_update > graph_update_delay) { //throttle redraw to 20Hz
-              for (var i = 0; i < 8; i++) {
-                  update_bar_css(i, 'Fz', $rootScope.state.control[0] * eepromConfig.mixTableFz[i] / Math.max.apply(null, eepromConfig.mixTableFz));
-                  update_bar_css(i, 'Tx', $rootScope.state.control[1] * eepromConfig.mixTableTx[i] / Math.max.apply(null, eepromConfig.mixTableTx));
-                  update_bar_css(i, 'Ty', $rootScope.state.control[2] * eepromConfig.mixTableTy[i] / Math.max.apply(null, eepromConfig.mixTableTy));
-                  update_bar_css(i, 'Tz', $rootScope.state.control[3] * eepromConfig.mixTableTz[i] / Math.max.apply(null, eepromConfig.mixTableTz));
-                  update_bar_css(i, 'sum', $rootScope.state.MotorOut[i]);
-                  if (!$('.'+ i +' .motor-view-level-value').is(":focus"))
-                      $('.'+ i +' .motor-view-level-value').val($rootScope.state.MotorOut[i].toFixed(0));
-              }
-              last_motors_view_update = now;
-          }
+        var last_motors_view_update = new Date();
+        $scope.$watch('state', function() {
+            if (!$rootScope.state)
+                return;
 
-      });
+            $scope.motorsEnabledNeg = !($rootScope.state.status & 0x0400);
+            $scope.motorsOverrideNeg = !($rootScope.state.status & 0x8000);
 
-      $scope.motorsEnabledNeg = true;
-      $scope.motorsOverrideNeg = true;
-  };
+            var now = new Date();
+            if (now - last_motors_view_update > graph_update_delay) {  // throttle redraw to 20Hz
+                for (var i = 0; i < 8; i++) {
+                    update_bar_css(i, 'Fz', $rootScope.state.control[0] * $rootScope.eepromConfig.mixTableFz[i] / Math.max.apply(null, $rootScope.eepromConfig.mixTableFz));
+                    update_bar_css(i, 'Tx', $rootScope.state.control[1] * $rootScope.eepromConfig.mixTableTx[i] / Math.max.apply(null, $rootScope.eepromConfig.mixTableTx));
+                    update_bar_css(i, 'Ty', $rootScope.state.control[2] * $rootScope.eepromConfig.mixTableTy[i] / Math.max.apply(null, $rootScope.eepromConfig.mixTableTy));
+                    update_bar_css(i, 'Tz', $rootScope.state.control[3] * $rootScope.eepromConfig.mixTableTz[i] / Math.max.apply(null, $rootScope.eepromConfig.mixTableTz));
+                    update_bar_css(i, 'sum', $rootScope.state.MotorOut[i]);
+                    if (!$('.' + i + ' .motor-view-level-value').is(":focus"))
+                        $('.' + i + ' .motor-view-level-value').val($rootScope.state.MotorOut[i].toFixed(0));
+                }
+                last_motors_view_update = now;
+            }
 
-  angular.module('flybrixApp').controller('motorsController', ['$scope', '$rootScope', motorsController]);
+        });
+
+        $scope.overridePilot = function() {
+            serial.send(CommandFields.COM_SET_COMMAND_OVERRIDE, ($scope.motorsOverrideNeg ? 1 : 0));
+        };
+
+        $scope.enableMotors = function() {
+            if ($scope.motorsEnabledNeg) {
+                console.log('enabling motors');
+                var i = 0;
+                $interval(function() {
+                    serial.send(CommandFields.COM_REQ_ENABLE_ITERATION, 1, i++ === 81);
+                }, 10, 82);
+            } else {
+                console.log('disabling motors');
+                serial.send(CommandFields.COM_REQ_ENABLE_ITERATION, 0);
+            }
+        };
+
+        $scope.zeroMotors = function() {
+            console.log('zero motors');
+            serial.send(CommandFields.COM_MOTOR_OVERRIDE_SPEED_ALL, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        };
+
+        $scope.motorsEnabledNeg = true;
+        $scope.motorsOverrideNeg = true;
+    };
+
+    angular.module('flybrixApp').controller('motorsController', ['$scope', '$rootScope', '$interval', 'serial', motorsController]);
 }());
