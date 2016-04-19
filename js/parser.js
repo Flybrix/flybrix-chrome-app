e1c8d434882d57ad576ce43f235a043572c5f463
Adnan Ademovic <adnanademovic100@gmail.com>
2016-04-19 11:14:37 +0200

    Fix the state data mask

--- js/parser.js
+++ js/parser.js
@@ -119,10 +119,10 @@ var CommandFields;  // TODO: wrap this into the IIFE as well
             var serial_update_rate_Hz = 0;
 
             for (var i = 0; i < state_data_mask.length; i++)
-                state_data_mask.push(0);
+                state_data_mask.push(false);
 
             if (0 != (mask & StateFields.STATE_MICROS)) {
-                state_data_mask[0] = 1;
+                state_data_mask[0] = true;
                 state.timestamp_us = data.getUint32(b.index, 1);
                 b.add(4);
 
@@ -130,115 +130,115 @@ var CommandFields;  // TODO: wrap this into the IIFE as well
                 last_timestamp_us = state.timestamp_us;
             }
             if (0 != (mask & StateFields.STATE_STATUS)) {
-                state_data_mask[1] = 1;
+                state_data_mask[1] = true;
                 state.status = data.getUint16(b.index, 1);
                 b.add(2);
             }
             if (0 != (mask & StateFields.STATE_V0)) {
-                state_data_mask[2] = 1;
+                state_data_mask[2] = true;
                 state.V0_raw = data.getUint16(b.index, 1);
                 b.add(2);
             }
             if (0 != (mask & StateFields.STATE_I0)) {
-                state_data_mask[3] = 1;
+                state_data_mask[3] = true;
                 state.I0_raw = data.getUint16(b.index, 1);
                 b.add(2);
             }
             if (0 != (mask & StateFields.STATE_I1)) {
-                state_data_mask[4] = 1;
+                state_data_mask[4] = true;
                 state.I1_raw = data.getUint16(b.index, 1);
                 b.add(2);
             }
             if (0 != (mask & StateFields.STATE_ACCEL)) {
-                state_data_mask[5] = 1;
+                state_data_mask[5] = true;
                 parseFloat32Array(data, state.accel, b);
             }
             if (0 != (mask & StateFields.STATE_GYRO)) {
-                state_data_mask[6] = 1;
+                state_data_mask[6] = true;
                 parseFloat32Array(data, state.gyro, b);
             }
             if (0 != (mask & StateFields.STATE_MAG)) {
-                state_data_mask[7] = 1;
+                state_data_mask[7] = true;
                 parseFloat32Array(data, state.mag, b);
             }
             if (0 != (mask & StateFields.STATE_TEMPERATURE)) {
-                state_data_mask[8] = 1;
+                state_data_mask[8] = true;
                 state.temperature = data.getUint16(b.index, 1) / 100.0;  // temperature
                 b.add(2);
             }
             if (0 != (mask & StateFields.STATE_PRESSURE)) {
-                state_data_mask[9] = 1;
+                state_data_mask[9] = true;
                 state.pressure = data.getUint32(b.index, 1) / 256.0;  // pressure (Q24.8)
                 b.add(4);
             }
             if (0 != (mask & StateFields.STATE_RX_PPM)) {
-                state_data_mask[10] = 1;
+                state_data_mask[10] = true;
                 parseInt16Array(data, state.ppm, b);
             }
             if (0 != (mask & StateFields.STATE_AUX_CHAN_MASK)) {
-                state_data_mask[11] = 1;
+                state_data_mask[11] = true;
                 state.AUX_chan_mask = data.getUint8(b.index, 1);
                 b.add(1);
             }
             if (0 != (mask & StateFields.STATE_COMMANDS)) {
-                state_data_mask[12] = 1;
+                state_data_mask[12] = true;
                 parseInt16Array(data, state.command, b);
             }
             if (0 != (mask & StateFields.STATE_F_AND_T)) {
-                state_data_mask[13] = 1;
+                state_data_mask[13] = true;
                 parseFloat32Array(data, state.control, b);
             }
             if (0 != (mask & StateFields.STATE_PID_FZ_MASTER)) {
-                state_data_mask[15] = 1;
+                state_data_mask[15] = true;
                 parse_pid_data(data, state.pid_master_Fz, b);
             }
             if (0 != (mask & StateFields.STATE_PID_TX_MASTER)) {
-                state_data_mask[16] = 1;
+                state_data_mask[16] = true;
                 parse_pid_data(data, state.pid_master_Tx, b);
             }
             if (0 != (mask & StateFields.STATE_PID_TY_MASTER)) {
-                state_data_mask[17] = 1;
+                state_data_mask[17] = true;
                 parse_pid_data(data, state.pid_master_Ty, b);
             }
             if (0 != (mask & StateFields.STATE_PID_TZ_MASTER)) {
-                state_data_mask[18] = 1;
+                state_data_mask[18] = true;
                 parse_pid_data(data, state.pid_master_Tz, b);
             }
             if (0 != (mask & StateFields.STATE_PID_FZ_SLAVE)) {
-                state_data_mask[19] = 1;
+                state_data_mask[19] = true;
                 parse_pid_data(data, state.pid_slave_Fz, b);
             }
             if (0 != (mask & StateFields.STATE_PID_TX_SLAVE)) {
-                state_data_mask[20] = 1;
+                state_data_mask[20] = true;
                 parse_pid_data(data, state.pid_slave_Tx, b);
             }
             if (0 != (mask & StateFields.STATE_PID_TY_SLAVE)) {
-                state_data_mask[21] = 1;
+                state_data_mask[21] = true;
                 parse_pid_data(data, state.pid_slave_Ty, b);
             }
             if (0 != (mask & StateFields.STATE_PID_TZ_SLAVE)) {
-                state_data_mask[22] = 1;
+                state_data_mask[22] = true;
                 parse_pid_data(data, state.pid_slave_Tz, b);
             }
             if (0 != (mask & StateFields.STATE_MOTOR_OUT)) {
-                state_data_mask[23] = 1;
+                state_data_mask[23] = true;
                 parseInt16Array(data, state.MotorOut, b);
             }
             if (0 != (mask & StateFields.STATE_KINE_ANGLE)) {
-                state_data_mask[24] = 1;
+                state_data_mask[24] = true;
                 parseFloat32Array(data, state.kinematicsAngle, b);
             }
             if (0 != (mask & StateFields.STATE_KINE_RATE)) {
-                state_data_mask[25] = 1;
+                state_data_mask[25] = true;
                 parseFloat32Array(data, state.kinematicsRate, b);
             }
             if (0 != (mask & StateFields.STATE_KINE_ALTITUDE)) {
-                state_data_mask[26] = 1;
+                state_data_mask[26] = true;
                 state.kinematicsAltitude = data.getFloat32(b.index, 1);
                 b.add(4);
             }
             if (0 != (mask & StateFields.STATE_LOOP_COUNT)) {
-                state_data_mask[27] = 1;
+                state_data_mask[27] = true;
                 state.loopCount = data.getUint32(b.index, 1);
                 b.add(4);
             }
