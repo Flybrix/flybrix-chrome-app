ef033e6a7c74c081066a7fa9795bddba611ca436
Adnan Ademovic <adnanademovic100@gmail.com>
2017-01-11 23:43:43 +0100

    Update LED tab to new API

--- tabs/led.js
+++ tabs/led.js
@@ -1,71 +1,48 @@
 (function() {
     'use strict';
 
-    var ledController = function($scope, serial) {
+    var ledController = function($scope, led) {
         function hexToRgb(hex) {
-            var rgb = /^#([\dA-Fa-f]{2})([\dA-Fa-f]{2})([\dA-Fa-f]{2})$/.exec(hex);
+            var rgb =
+                /^#([\dA-Fa-f]{2})([\dA-Fa-f]{2})([\dA-Fa-f]{2})$/.exec(hex);
             if (!rgb)
                 rgb = [null, '0', '0', '0'];
             return {
-                r: parseInt(rgb[1], 16),
-                g: parseInt(rgb[2], 16),
-                b: parseInt(rgb[3], 16),
+                red: parseInt(rgb[1], 16),
+                green: parseInt(rgb[2], 16),
+                blue: parseInt(rgb[3], 16),
             };
         }
 
-        $scope.$watch('led', function() {
-            updateColors();
-        }, true);
+        $scope.ledState = {
+            mode: 5,
+            front_left: '#ffffff',
+            front_right: '#ffffff',
+            rear_left: '#ffffff',
+            rear_right: '#ffffff',
+            red: false,
+            green: false,
+        };
 
-        function updateColors() {
-            var l = hexToRgb($scope.led.light.left);
-            var r = hexToRgb($scope.led.light.right);
-            var message = new Uint8Array([
-                $scope.led.mode,
-                r.r,
-                r.g,
-                r.b,
-                l.r,
-                l.g,
-                l.b,
-                $scope.led.indicator.red,
-                $scope.led.indicator.green,
-            ]);
-            serial.send(serial.field.COM_SET_LED, message, false)
-                .then(
-                    function() {
-                        console.log("Successful serial message");
-                    },
-                    function(value) {
-                        console.log("Failed serial message", "PROBLEM:", value);
-                    });
-        }
+        $scope.$watchCollection('ledState', function(v) {
+            led.set(
+                hexToRgb(v.front_right), hexToRgb(v.rear_right),
+                hexToRgb(v.front_left), hexToRgb(v.rear_left), v.mode, v.red,
+                v.green);
+        });
 
         $scope.ledModes = [
-            {name: 'No override', code: 0},
             {name: 'Flash', code: 1},
             {name: 'Beacon', code: 2},
             {name: 'Breathe', code: 3},
             {name: 'Alternate', code: 4},
             {name: 'Solid light', code: 5},
         ];
-
-        $scope.led = {
-            mode: 0,
-            indicator: {
-                green: false,
-                red: false,
-            },
-            light: {
-                left: '#000000',
-                right: '#000000',
-            },
-        };
     };
 
     var app = angular.module('flybrixApp');
 
-    app.controller('ledController', ['$scope', 'serial', ledController]);
+    app.controller('ledController', ['$scope', 'led', ledController]);
 
     app.directive('colorPicker', function() {
         var link = function(scope, element, attr, ngModel) {
@@ -73,17 +50,18 @@
 
             element.spectrum({
                 flat: true,
-                preferredFormat: "hex",
+                preferredFormat: 'hex',
                 showInput: true,
                 showButtons: false,
-                color: "black",
+                color: 'black',
                 clickoutFiresChange: false,
                 change: function(color) {
                     ngModel.$setViewValue('#' + color.toHex());
                 },
                 move: function(color) {
                     var newTime = new Date();
-                    if (newTime - latestUpdate < 50) {  // up to 20 updates per second
+                    if (newTime - latestUpdate <
+                        50) {  // up to 20 updates per second
                         return;
                     }
                     latestUpdate = newTime;
